a864a31ccf88b12926c538c7bccfc30f153a62db
Anthony Shull <shull.anthony@gmail.com>
2024-02-06 16:55:54 -0600

    initial commit

---
+++ lib/telemetry_metrics_splunk.ex
@@ -0,0 +1,18 @@
+defmodule TelemetryMetricsSplunk do
+  @moduledoc """
+  Documentation for `TelemetryMetricsSplunk`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TelemetryMetricsSplunk.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/telemetry_metrics_splunk_test.exs
@@ -0,0 +1,8 @@
+defmodule TelemetryMetricsSplunkTest do
+  use ExUnit.Case
+  doctest TelemetryMetricsSplunk
+
+  test "greets the world" do
+    assert TelemetryMetricsSplunk.hello() == :world
+  end
+end
